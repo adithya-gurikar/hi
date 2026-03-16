4e736def1a698c30957ea891c6a4b00fa348232a
Adithya Gurikar <Adithya.Gurikar@ELIGARF.TECH>
2026-03-16 16:00:11 +0530

    test update

--- test/test_op.py
+++ test/test_op.py
@@ -3,7 +3,7 @@ from src.math_op import add,sub
 def test_add():
     assert add(2,3) == 5
     assert add (1,2) == 3
-    assert add (1,1) == 2
+    assert add (1,1) == 0
     
 def test_sub():
     assert sub(5,3) == 2
